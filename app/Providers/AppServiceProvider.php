c98fdcdaca10b4abdf42baca08ef845cd273c2d1
ChinonsoIke <chinonsoikewelugo@gmail.com>
2020-08-10 23:30:59 +0100

    modified service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use View;
 use Schema;
+use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+
+        if($this->app->environment('production')) {
+            URL::forceSchema('https');
+        }
 
         Schema::defaultStringLength(191);
 
